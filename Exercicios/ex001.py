42fb6b8e0c34bb1a6f61145e2af0855a911d6ef3
Beatriz Fernandes <biafernandesilva1808@gmail.com>
2026-05-26 17:52:47 -0300

    Atualização 2

---
+++ Exercicios/ex001.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+# Este é o meu primeiro programa em Python
+
+msg = 'Olá, Mundo!'
+print(msg)
---
+++ Exercicios/ex002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome:')
+#print(f'Olá, {nome}! Seja Bem-Vindo(a) aos Exercicios de Python!')
+
+# Outra forma de fazer:
+print('Olá, {}! Seja Bem-Vindo(a) aos Exercicios de Python!'.format(nome))
